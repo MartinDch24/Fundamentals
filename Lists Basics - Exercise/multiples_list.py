35d5ac5746e8d66c3d39f13eb38337a37fd45714
MartinDch24 <144941576+MartinDch24@users.noreply.github.com>
2023-11-27 23:45:40 +0200

    Delete Lists Basics - Exercise directory

--- Lists Basics - Exercise/multiples_list.py
+++
@@ -1,7 +0,0 @@
-factor = int(input())
-count = int(input())
-multiples_of_the_factor = []
-
-for number in range(1, count + 1):
-    multiples_of_the_factor.append(number*factor)
-print(multiples_of_the_factor)

3bec09192dc1514e397a75b1fe6b412363effb04
MartinDch24 <144941576+MartinDch24@users.noreply.github.com>
2023-11-27 23:45:06 +0200

    Delete Functions - Exercise directory

--- Functions - Exercise/loading_bar.py
+++
@@ -1,9 +0,0 @@
-def loading_bar(number)-> str:
-    if number == 100:
-        return f"{number}% Complete!\n[%%%%%%%%%%]"
-    loaded_percentage = number // 10
-    return f"{number}% [{'%' * loaded_percentage}{'.' * (10 - loaded_percentage)}]\nStill loading..."
-
-
-percentage = int(input())
-print(loading_bar(percentage))
--- Functions - Exercise/palindrome_integers.py
+++
@@ -1,9 +0,0 @@
-def is_palindrome(number: str) -> bool:
-    if number == number[::-1]:
-        return True
-    return False
-
-
-list_of_numbers = input().split(", ")
-for list_of_numbers in list_of_numbers:
-    print(is_palindrome(list_of_numbers))
